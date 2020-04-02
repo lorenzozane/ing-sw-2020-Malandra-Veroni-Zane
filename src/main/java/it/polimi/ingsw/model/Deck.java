c292f01b010f9e9b9cb9b113cf1238d51db7d67b
Lorenzo Zane <lorenzo.zane@mail.polimi.it>
2020-04-02 02:16:38 +0200

    Completato buildDeck. Aggiunto test di buildDeck.

--- src/main/java/it/polimi/ingsw/model/Deck.java
+++ src/main/java/it/polimi/ingsw/model/Deck.java
@@ -5,7 +5,6 @@ import java.io.File;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
-import com.sun.org.apache.xerces.internal.dom.DocumentImpl;
 import org.w3c.dom.Document;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
@@ -14,7 +13,6 @@ import org.w3c.dom.Element;
 
 public class Deck {
 
-    //Verificare Warning "Raw use of parameterized class 'ArrayList'"
     private ArrayList<GodsCard> cardList = new ArrayList<>();
     private ArrayList<GodsCard> chosenCards = new ArrayList<>();
 
@@ -24,20 +22,24 @@ public class Deck {
 
     private void buildDeck(){
         try {
+            //Apertura file xml GodsDescription.xml, ed inizializzazione documento
             File xmlGodsDescription = new File("src/GodsDescription.xml");
             DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
             DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
             Document document = documentBuilder.parse(xmlGodsDescription);
+            //Normalizza gli elementi del file nel caso ci fossero "problemi" di formattazione
             document.getDocumentElement().normalize();
 
+            //Salva il nodo root e cerca tutti i nodi figli (le divinità)
             Element rootElement = document.getDocumentElement();
             NodeList godsNameNode = rootElement.getChildNodes();
 
+            //Crea un nuovo nodo singolo per ogni divinità e istanzia una carta aggiungendola a cardList
             for (int i = 0; i < godsNameNode.getLength(); i++){
                 Node godNode = godsNameNode.item(i);
                 if (godNode.getNodeType() == Node.ELEMENT_NODE){
                     GodsCard card = new GodsCard(godNode.getNodeName());
-                    //TODO;
+                    cardList.add(card);
                 }
             }
 
@@ -53,5 +55,9 @@ public class Deck {
 
     public void chooseCards(){
 
+    }
+
+    public ArrayList<GodsCard> getCardList(){
+        return cardList;
     }
 }
--- src/test/java/it/polimi/ingsw/model/DeckTest.java
+++ src/test/java/it/polimi/ingsw/model/DeckTest.java
@@ -2,6 +2,13 @@ package it.polimi.ingsw.model;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.anyOf;
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class DeckTest {
@@ -9,6 +16,15 @@ public class DeckTest {
     @Test
     public void buildDeckTest(){
         Deck deck = new Deck();
+        ArrayList<GodsCard> cardList = deck.getCardList();
+        List<String> godsName = new LinkedList<String>(Arrays.asList("apollo", "artemis", "athena", "atlas", "demeter", "hephaestus", "minotaur", "pan", "prometheus"));
+
+        assertEquals(cardList.size(), godsName.size());
+
+        for (int i = 0; i < cardList.size(); i++){
+            assertTrue(godsName.contains(cardList.get(i).getCardName().toString()));
+            godsName.remove(cardList.get(i).getCardName());
+        }
     }
 
 }
