612f03024cc63228ceb557f506ba4b796e4c46b8
micheleveroni <61704973+micheleveroni@users.noreply.github.com>
2020-04-03 12:27:14 +0200

    completed addPlayer completed checkPlayer completed removePlayer started challenge (serve il network)

--- src/main/java/it/polimi/ingsw/model/Game.java
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.model;
 
 import java.util.ArrayList;
+import javax.swing.DefaultListSelectionModel;
 
 public class Game {
 
@@ -8,6 +9,11 @@ public class Game {
     private int playerNumber;
     private Board board;
     private Deck godsDeck;
+    private Player firstPlayer;
+
+    public Game() {
+
+    }
 
 
     public int getPlayerNumber() {
@@ -17,4 +23,69 @@ public class Game {
     public void setPlayerNumber(int playerNumber) {
         this.playerNumber = playerNumber;
     }
+
+    public boolean addPlayer(Player newPlayer) {
+        if(checkPlayer(newPlayer)) {
+            playerList.add(newPlayer);
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+    private boolean checkPlayer(Player newPlayer){
+        for (int counter = 0; counter < playerList.size(); counter++) {
+            if (playerList.contains(newPlayer) )
+                return false;
+        }
+        return true;
+    }
+
+    public void removePlayer(final Player playerToDelete){
+        playerList.remove(playerToDelete);
+        throw new IllegalArgumentException();
+    }
+
+    public Player getChallengerPlayer(){
+        return this.firstPlayer;
+    }
+
+    public void challenge(){
+        ArrayList<GodsCard> challengeCard = new ArrayList<>();
+        if(playerNumber == 3){
+            while(challengeCard.size()!=3){
+                //chiedere al firstPlayer di selezionare una carta -- comunicazione server-client mancante
+                //challengeCard.add( carta selezionata dal challenger );
+            }
+            /* far scegliere una carta al
+            new card = playerList.get(1).choosecard
+            playerList.get(1).setGodsCard( card );
+            poi rimuoverla dalla lista
+            challengeCard.remove( card );
+            ripetere
+            card = playerList.get(2).choosecard
+            playerList.get(2).setGodsCard( card );
+            poi rimuoverla dalla lista
+            challengeCard.remove( card );
+            playerList.get(0).setGodsCard( challengeCard.get(0) );
+            */
+        }
+        else if (playerNumber == 2){
+            /* far scegliere una carta all'altro giocatore
+            new card = playerList.get(1).choosecard
+            playerList.get(1).setGodsCard( card );
+            poi rimuoverla dalla lista
+            challengeCard.remove( card );
+            playerList.get(0).setGodsCard( challengeCard.get(0) );
+            */
+        }
+        else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+
+
 }
