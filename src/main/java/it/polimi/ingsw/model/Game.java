75fa3321b060ac20f273e35231c6e3b6486955e1
micheleveroni <61704973+micheleveroni@users.noreply.github.com>
2020-04-05 14:35:00 +0200

    fix minori

--- src/main/java/it/polimi/ingsw/model/Game.java
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -47,7 +47,14 @@ public class Game {
         return this.firstPlayer;
     }
 
-    public void challenge(){
+    public void challenge(){ //il controller ha gia popolato Game.godsDeck con 2/3 carte scelte dal challenger
+        for(int i = 1; i<playerList.size(); i++){    //parto da 1 cosi il secondo giocatore piu giovane sceglie
+            //notificare alla view di mostrare le carte disponibili
+
+
+
+        }
+
 
     }
 
