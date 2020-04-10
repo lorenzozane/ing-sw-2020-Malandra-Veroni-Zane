a8e0d071c9c8d5e91c623b31ddcdc7e361f5d854
matteomalandra <matteo.malandra@mail.polimi.it>
2020-04-10 12:18:46 +0200

    board

--- src/main/java/it/polimi/ingsw/model/Board.java
+++ src/main/java/it/polimi/ingsw/model/Board.java
@@ -1,9 +1,11 @@
 package it.polimi.ingsw.model;
 
 public class Board {
-
-    private final int BOARD_DIMENSION = 5;
+    private final int BOARD_DIMENSION=5;
 
     private Slot[][] table;
 
+    public Board(){
+        table= new Slot[BOARD_DIMENSION][BOARD_DIMENSION];
+    }
 }
