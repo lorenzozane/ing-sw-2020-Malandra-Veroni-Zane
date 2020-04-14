ca4ac7d50775dd1a8f929711ed056f7c0a27da38
matteomalandra <matteo.malandra@mail.polimi.it>
2020-04-14 16:14:39 +0200

    color class

---
+++ src/main/java/it/polimi/ingsw/model/Color.java
@@ -0,0 +1,30 @@
+package it.polimi.ingsw.model;
+
+public enum Color {
+
+
+    ANSI_BLUE("\u001B[34m"),
+    ANSI_PURPLE("\u001B[35m"),
+    ANSI_BRIGHT_BLUE("\u001b[34;1m"),
+    ANSI_BRIGHT_CYAN("\u001b[36;1m");
+
+
+
+    static final String RESET = "\u001B[0m";
+    private String escape;
+
+    Color(String escape)
+    {
+        this.escape = escape;
+    }
+    public String getEscape()
+    {
+        return escape;
+
+    }
+    @Override
+    public String toString()
+    {
+        return escape;
+    }
+}
