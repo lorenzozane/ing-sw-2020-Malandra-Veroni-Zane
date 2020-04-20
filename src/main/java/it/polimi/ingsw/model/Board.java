f40f06fb9840d4f19c3febb73037d40bafe79b25
micheleveroni <61704973+micheleveroni@users.noreply.github.com>
2020-04-20 12:13:42 +0200

    fix windows worker pants

--- src/main/java/it/polimi/ingsw/model/Board.java
+++ src/main/java/it/polimi/ingsw/model/Board.java
@@ -228,14 +228,14 @@ public class Board implements Cloneable {
 
         switch (Arrays.toString(value)) {
             case "[0, 0, 0, 0]":
-                cube[5][0] = slotMove.getWorkerInSlot().getColor() + "        / \\         " + Color.RESET;
+                cube[5][0] = slotMove.getWorkerInSlot().getColor() + "        /¯\\         " + Color.RESET;
                 cube[4][0] = slotMove.getWorkerInSlot().getColor() + "        /|\\         " + Color.RESET;
                 cube[3][0] = slotMove.getWorkerInSlot().getColor() + "         o          " + Color.RESET;
                 break;
 
             case "[1, 0, 0, 0]":
                 cube[5][0] = "████████████████████";
-                cube[4][0] = slotMove.getWorkerInSlot().getColor() + "        / \\         " + Color.RESET;
+                cube[4][0] = slotMove.getWorkerInSlot().getColor() + "        /¯\\         " + Color.RESET;
                 cube[3][0] = slotMove.getWorkerInSlot().getColor() + "        /|\\         " + Color.RESET;
                 cube[2][0] = slotMove.getWorkerInSlot().getColor() + "         o          " + Color.RESET;
                 break;
@@ -243,7 +243,7 @@ public class Board implements Cloneable {
             case "[1, 2, 0, 0]":
                 cube[5][0] = "████████████████████";
                 cube[4][0] = "  ████████████████  ";
-                cube[3][0] = slotMove.getWorkerInSlot().getColor() + "        / \\         " + Color.RESET;
+                cube[3][0] = slotMove.getWorkerInSlot().getColor() + "        /¯\\         " + Color.RESET;
                 cube[2][0] = slotMove.getWorkerInSlot().getColor() + "        /|\\         " + Color.RESET;
                 cube[1][0] = slotMove.getWorkerInSlot().getColor() + "         o          " + Color.RESET;
                 break;
@@ -252,7 +252,7 @@ public class Board implements Cloneable {
                 cube[5][0] = "████████████████████";
                 cube[4][0] = "  ████████████████  ";
                 cube[3][0] = "    ████████████    ";
-                cube[2][0] = slotMove.getWorkerInSlot().getColor() + "        / \\         " + Color.RESET;
+                cube[2][0] = slotMove.getWorkerInSlot().getColor() + "        /¯\\         " + Color.RESET;
                 cube[1][0] = slotMove.getWorkerInSlot().getColor() + "        /|\\         " + Color.RESET;
                 cube[0][0] = slotMove.getWorkerInSlot().getColor() + "         o          " + Color.RESET;
                 break;
