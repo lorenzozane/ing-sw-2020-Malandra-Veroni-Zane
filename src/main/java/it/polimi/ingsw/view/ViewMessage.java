26861d0aff3ef2000380a729cae2479dad212000
matteomalandra <matteo.malandra@mail.polimi.it>
2020-05-21 11:01:49 +0200

    ViewMessage

--- src/main/java/it/polimi/ingsw/view/ViewMessage.java
+++ src/main/java/it/polimi/ingsw/view/ViewMessage.java
@@ -1,4 +1,26 @@
 package it.polimi.ingsw.view;
 
 public class ViewMessage {
+
+    //
+    public static String colorRequest;
+    public static String pickLastColor;
+    public static String chooseCardRequest;
+    public static String pickUpCardRequest;
+    public static String pickLastCard;
+    public static String placeWorker;
+
+    //MOVE
+    public static String moveStandard;
+    public static String moveNotInitialPosition;
+    public static String moveOpponentSlotFlip;
+    public static String moveOpponentSlotPush;
+    public static String moveDisableOpponentUp;
+
+    //BUILD
+    public static String buildStandard = "Build a building in a box adjacent to your worker";
+    public static String buildBefore = "Build a building in a box adjacent to your worker";
+    public static String buildNotSamePlace;
+    public static String buildSamePlaceNotDome;
+    public static String buildDomeAnyLevel;
 }
