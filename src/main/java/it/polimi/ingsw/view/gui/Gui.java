1257e1288146bd2a6715a80af32f349da487fc7f
matteomalandra <matteo.malandra@mail.polimi.it>
2020-06-02 22:24:10 +0200

    gui

--- src/main/java/it/polimi/ingsw/view/gui/Gui.java
+++ src/main/java/it/polimi/ingsw/view/gui/Gui.java
@@ -5,8 +5,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+
 
 public class Gui extends Application {
+
+
+
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("src/main/resources/GuiStyle.fxml"));
@@ -16,6 +22,7 @@ public class Gui extends Application {
     }
 
     public static void main(String[] args) {
+
         launch(args);
     }
 }
